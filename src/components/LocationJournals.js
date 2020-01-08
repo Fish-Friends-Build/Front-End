2c3b3b9008faa18885d883f510aeb34597eb4623
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-01-07 18:56:01 -0800

    Created LocationJournals component. Linked to specific component from FishingSpotsCard.js

---
+++ src/components/LocationJournals.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const LocationJournals = props => {
+    return <p>here will be all existing journals of location</p>;
+}
+
+export default LocationJournals;
