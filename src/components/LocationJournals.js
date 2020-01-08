81c16f5a198d3cf0304935a6f0881d3432148239
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-01-07 19:29:22 -0800

    Created component to view all journal entries of a specific location... work in progress

--- src/components/LocationJournals.js
+++ src/components/LocationJournals.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 
 import axiosWithAuth from '../utils/axiosWithAuth';
 
 const LocationJournals = props => {
-    return <p>here will be all existing journals of location</p>;
+    const [journals, setJournals] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/journals')
+            .then(res => {
+                console.log(res.data);
+
+                let journalGet = [];
+                // console.log(props.match.params.name);
+                for (let i = 0; i < res.data.length; i++) {
+                    if (res.data.location === props.match.params.name) {
+                        console.log(res.data[i]);
+                        journalGet.push(res.data[i]);
+                    }
+                }
+                console.log(journalGet);
+                setJournals(journalGet);
+
+            })
+            .catch(err => console.log("LocationJournals get error: ", err));
+    }, []);
+
+    return (
+        <>
+            {journals.map(entry => (
+                <p>Location Name: {entry.location}</p>
+            ))}
+        </>
+    )
 }
 
 export default LocationJournals;
