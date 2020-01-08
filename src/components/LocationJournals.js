e2967ae88e84fb0847e35adde1c5ec8d001b5b72
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-01-08 00:40:50 -0800

    Users can now globally view all posts when viewing a specific location

--- src/components/LocationJournals.js
+++ src/components/LocationJournals.js
@@ -1,35 +1,58 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import axiosWithAuth from '../utils/axiosWithAuth';
 
 const LocationJournals = props => {
-    const [journals, setJournals] = useState([]);
+    const [journals, setJournals] = useState([{
+        numFishCaught: null,
+        date: "",
+        timeOfDay: "",
+        location: "",
+        fishType: [""],
+        bait: "",
+        bankOrBoat: "",
+        waterType: "",
+        notes: null,
+        username: ""
+    }]);
 
     useEffect(() => {
         axiosWithAuth()
             .get('/journals')
             .then(res => {
-                console.log(res.data);
-
                 let journalGet = [];
                 // console.log(props.match.params.name);
                 for (let i = 0; i < res.data.length; i++) {
-                    if (res.data.location === props.match.params.name) {
-                        console.log(res.data[i]);
+                    if (res.data[i].location === props.match.params.name) {
+                        // console.log(res.data[i]);
                         journalGet.push(res.data[i]);
                     }
                 }
-                console.log(journalGet);
+                // console.log(journalGet);
                 setJournals(journalGet);
 
             })
             .catch(err => console.log("LocationJournals get error: ", err));
     }, []);
 
+    console.log(journals);
+
     return (
         <>
             {journals.map(entry => (
-                <p>Location Name: {entry.location}</p>
+                <div className="global-journal-cards">
+                    <h4>Location Name: {entry.location}</h4>
+                    <p>Date: {entry.date}</p>
+                    <p>Time of Day: {entry.timeOfDay}</p>
+                    <p>Number of Fish Caught: {entry.numFishCaught}</p>
+                    <p>Fish Type: {entry.fishType}</p>
+                    <p>Bait Type: {entry.bait}</p>
+                    <p>Bank or Boat: {entry.bankOrBoat}</p>
+                    <p>Fresh Water or Salt Water: {entry.waterType}</p>
+                    <p>NOTES: {entry.notes}</p>
+                    <p>posted by: {entry.username}</p>
+                </div>
+
             ))}
         </>
     )
